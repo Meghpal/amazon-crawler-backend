56d2dd6169ec8c1b1f697821d7f27b2ad04322a5
Meghpal <meghpal.s@gmail.com>
2020-11-12 23:12:07 +0530

    improved file-write

--- helpers/save_data.py
+++ helpers/save_data.py
@@ -1,4 +1,4 @@
 def save_data(obj, id, mode):
     f = open("./reviews/reviews_" + id + ".txt", mode, encoding="utf-8")
-    f.write(str(obj))
+    f.write("\n".join(map(lambda x: repr(x)[1:-1], obj)))
     f.close()
